efdbe4a9b91d8a915f0ed5f39df7ddfe22c34b45
adri <62965634+cuadriante@users.noreply.github.com>
2023-03-04 20:48:52 -0600

    test bench

---
+++ lab_02/p01/problema1_BCD.sv
@@ -0,0 +1,13 @@
+module problema1_BCD(input logic I0, I1, I2, I3, I4, b, 
+				  output logic S0, S1, S2, S3, S4);
+
+	// BCD ( S4 S3 S2 S1 S0 )
+	assign S4 = (I0&&I1)|(I0&&I2);
+	assign S3 = (I0&&!I1&&!I2);
+	assign S2 = (!I0&&I1)|(I1&&I2);
+	assign S1 = (I0&&I1&&!I2)|(!I0&&I2);
+	assign S0 = (I3);
+	
+
+	
+endmodule
