5d0c69298ca34929efbdb6317f1a9a2d4760f6d8
Nasser <nasserbrown@estudiantec.cr>
2023-06-11 23:48:45 -0600

    freqs

---
+++ lab3_p2/multiplexor.sv
@@ -0,0 +1,38 @@
+module multiplexor#(parameter N=32)(input [N-1:0] a, b, c,d, e, f, g, input [3:0] ss, output [N-1:0] salida);
+logic [N-1:0] aux;
+
+always @(ss) begin
+	case (ss)
+		0:
+			aux = a;
+
+		1:
+			aux = b;
+			
+		2:
+			aux = c;
+
+		3:
+			aux = d;
+
+			
+		4:
+			aux = e;
+
+		5:
+			aux = f;
+
+		6:
+			aux = g;
+
+
+	
+		default: aux = 0;
+	endcase
+	
+end
+
+assign salida = aux;	
+
+
+endmodule
